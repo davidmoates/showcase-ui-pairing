bab65a5abfaaf17dae0f4f70b165099b182b2835
davidmoates <david.m.moates@gmail.com>
2021-07-15 16:14:03 -0500

    created WikiCard.js and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import WikiCard from './components/WikiCard'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [urlInfo, setUrlInfo] = useState([])
+
+  const getData = () => {
+    axios
+        .get("https://api.wikimedia.org/core/v1/wikipedia/en/search/page?q=earth&limit=10")
+        .then(response => {
+          console.log(response.data.pages);
+          setUrlInfo(response.data.pages)
+          console.log(urlInfo)
+
+        })
+        .catch(error => {
+          console.log("Error:", error);
+        });
+        // setUrlInfo(
+        //   [{
+        //     key: 1,
+        //     title: 'hello world',
+        //     thumbnail: {url:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2xl8rfONRzFklWCCnXTsbwHaHC%26pid%3DApi&f=1'}
+        //     }
+        //   ]
+        // )
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Wikimedia image browser
-        </h1>
-      </header>
+    <div>
+      <button onClick={getData}>Click</button>
+        {urlInfo.map(img => {
+          return(
+            <div className="App">
+              <WikiCard
+                title={img.title}
+                url={img.thumbnail.url}/>
+            </div>
+            )
+          }
+        )
+      }
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
